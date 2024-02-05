a53034bbdfdb69d11c7f39e078163f735be5f0a7
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-05 23:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here!
+
+function fn(){
+	const element = document.getElementById("infi-list");
+let y = element.scrollHeight;
+	if(y === 700){
+	let li1 = document.createElement("li");
+	li.createTextNode("Item 11");
+		let li2 = document.createElement("li");
+	li2.createTextNode("Item 12");
+		element.appendChild(li1);
+		element.appendChild(li2);
+	}
+}
 
